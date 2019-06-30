fe089f72e716862e081a36b82943348d1a2c2c22
1g0rbm <m1g0rb89@gmail.com>
2019-07-01 00:06:38 +0300

    update difrection parser

--- src/Entity/Translation/Yandex/Direction.php
+++ src/Entity/Translation/Yandex/Direction.php
@@ -6,5 +6,66 @@ namespace Ig0rbm\Memo\Entity\Translation\Yandex;
 
 class Direction
 {
+    /**
+     * @var string
+     */
+    private $langFrom;
 
+    /**
+     * @var string
+     */
+    private $langTo;
+
+    /**
+     * @var string
+     */
+    private $direction;
+
+    /**
+     * @return string
+     */
+    public function getLangFrom(): string
+    {
+        return $this->langFrom;
+    }
+
+    /**
+     * @param string $langFrom
+     */
+    public function setLangFrom(string $langFrom): void
+    {
+        $this->langFrom = $langFrom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLangTo(): string
+    {
+        return $this->langTo;
+    }
+
+    /**
+     * @param string $langTo
+     */
+    public function setLangTo(string $langTo): void
+    {
+        $this->langTo = $langTo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDirection(): string
+    {
+        return $this->direction;
+    }
+
+    /**
+     * @param string $direction
+     */
+    public function setDirection(string $direction): void
+    {
+        $this->direction = $direction;
+    }
 }
--- src/Service/Translation/Yandex/DirectionParser.php
+++ src/Service/Translation/Yandex/DirectionParser.php
@@ -1,10 +1,20 @@
 <?php
 
-
 namespace Ig0rbm\Memo\Service\Translation\Yandex;
 
+use Ig0rbm\Memo\Entity\Translation\Yandex\Direction;
 
 class DirectionParser
 {
+    public function parse(string $rawDirection): Direction
+    {
+        [$langFrom, $langTo] = explode('-', $rawDirection);
 
+        $direction = new Direction();
+        $direction->setDirection($rawDirection);
+        $direction->setLangFrom($langFrom);
+        $direction->setLangTo($langTo);
+
+        return $direction;
+    }
 }
