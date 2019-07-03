19b59d401034a44a021f3239950dde47a659826c
1g0rbm <m1g0rb89@gmail.com>
2019-07-03 23:23:25 +0300

    fix translationAction

--- src/TelegramAction/TranslationAction.php
+++ src/TelegramAction/TranslationAction.php
@@ -6,6 +6,7 @@ use Ig0rbm\Memo\Entity\Telegram\Command\Command;
 use Ig0rbm\Memo\Entity\Telegram\Message\MessageFrom;
 use Ig0rbm\Memo\Entity\Telegram\Message\MessageTo;
 use Ig0rbm\Memo\Service\Telegram\TelegramApiService;
+use Ig0rbm\Memo\Service\Translation\DirectionParser;
 use Ig0rbm\Memo\Service\Translation\Yandex\YandexDictionaryApiService;
 
 class TranslationAction extends AbstractTelegramAction
@@ -13,18 +14,30 @@ class TranslationAction extends AbstractTelegramAction
     /** @var YandexDictionaryApiService */
     private $dictionaryApi;
 
-    public function __construct(TelegramApiService $api, YandexDictionaryApiService $dictionaryApi)
+    /** @var DirectionParser */
+    private $directionParser;
+
+    public function __construct(
+        TelegramApiService $api,
+        YandexDictionaryApiService $dictionaryApi,
+        DirectionParser $directionParser
+    )
     {
         parent::__construct($api);
 
         $this->dictionaryApi = $dictionaryApi;
+        $this->directionParser = $directionParser;
     }
 
     public function run(MessageFrom $messageFrom, Command $command): void
     {
-        $response = $this->dictionaryApi->getTranslate('en-ru', $messageFrom->getText()->getText());
+        $response = $this->dictionaryApi->getTranslate(
+            $this->directionParser->parse('en-ru'),
+            $messageFrom->getText()->getText()
+        );
+
         $messageTo = new MessageTo();
-        $messageTo->setText($response);
+        $messageTo->setText($response->getText());
         $messageTo->setChatId($messageFrom->getChat()->getId());
 
         $this->api->sendMessage($messageTo);
