bca72b41d9f1892d27e84ff02f61a5a1dbc5c3e9
1g0rbm <m1g0rb89@gmail.com>
2019-07-18 22:27:09 +0300

    Add more test

--- tests/Service/Translation/TranslationServiceUnitTest.php
+++ tests/Service/Translation/TranslationServiceUnitTest.php
@@ -2,13 +2,14 @@
 
 namespace Ig0rbm\Memo\Tests\Service\Translation;
 
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 use Ig0rbm\HandyBag\HandyBag;
 use Ig0rbm\Memo\Entity\Translation\Direction;
+use Ig0rbm\Memo\Entity\Translation\Text;
 use Ig0rbm\Memo\Service\Telegram\MessageBuilder;
 use Ig0rbm\Memo\Service\Translation\ApiTextTranslationInterface;
 use Ig0rbm\Memo\Service\Translation\Yandex\DictionaryParser;
-use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
 use Ig0rbm\Memo\Service\Translation\DirectionParser;
 use Ig0rbm\Memo\Service\Translation\TranslationService;
 use Ig0rbm\Memo\Service\Translation\ApiWordTranslationInterface;
@@ -55,7 +56,7 @@ class TranslationServiceUnitTest extends TestCase
             ->method('parse')
             ->willReturn($direction);
 
-        $wordCollection = $this->getWordsCollection($direction);
+        $wordCollection = $this->getWordsCollection($direction, $this->getDictionary());
         $this->apiWordTranslation->expects($this->once())
             ->method('getTranslate')
             ->with($direction, $wordForTranslate)
@@ -75,9 +76,50 @@ class TranslationServiceUnitTest extends TestCase
         );
     }
 
-    private function getWordsCollection(Direction $direction): HandyBag
+    public function testReturnValidTranslateForText(): void
     {
-        return (new DictionaryParser())->parse(json_encode($this->getDictionary()), $direction);
+        $wordForTranslate = 'test';
+        $direction = $this->getDirection();
+
+        $this->directionParser->expects($this->once())
+            ->method('parse')
+            ->willReturn($direction);
+
+        $wordCollection = $this->getWordsCollection($direction, $this->getEmptyDictionary());
+        $this->apiWordTranslation->expects($this->once())
+            ->method('getTranslate')
+            ->with($direction, $wordForTranslate)
+            ->willReturn($wordCollection);
+
+        $this->messageBuilder->expects($this->never())
+            ->method('buildFromWords');
+
+        $text = new Text();
+        $text->setText($wordForTranslate);
+        $this->apiTextTranslation->expects($this->once())
+            ->method('getTranslate')
+            ->with($direction, $wordForTranslate)
+            ->willReturn($text);
+
+        $this->messageBuilder->expects($this->once())
+            ->method('buildFromText')
+            ->with($text)
+            ->willReturn($wordForTranslate);
+
+        $this->assertSame($text->getText(), $this->service->translate('en-ru', $wordForTranslate));
+    }
+
+    private function getWordsCollection(Direction $direction, array $dictionary): HandyBag
+    {
+        return (new DictionaryParser())->parse(json_encode($dictionary), $direction);
+    }
+
+    private function getEmptyDictionary(): array
+    {
+        return [
+            'head' => [],
+            'def' => [],
+        ];
     }
 
     private function getDictionary(): array
