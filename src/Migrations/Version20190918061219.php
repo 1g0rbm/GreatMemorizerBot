27889355d1bea11df5e4b016a6440c8034574e2e
1g0rbm <m1g0rb89@gmail.com>
2019-09-18 09:47:01 +0300

    fix accounts.direction_id

---
+++ src/Migrations/Version20190918061219.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190918061219 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX uniq_cac89eacaf73d997');
+        $this->addSql('CREATE INDEX IDX_CAC89EACAF73D997 ON accounts (direction_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX IDX_CAC89EACAF73D997');
+        $this->addSql('CREATE UNIQUE INDEX uniq_cac89eacaf73d997 ON accounts (direction_id)');
+    }
+}
