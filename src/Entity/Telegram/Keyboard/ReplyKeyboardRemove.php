57f5edc7838a1c3db21e8a2246956fbbe91d2b46
1g0rbm <m1g0rb89@gmail.com>
2019-09-18 23:51:45 +0300

    Add smells command for deleting ReplyKeyboard for every chats

---
+++ src/Entity/Telegram/Keyboard/ReplyKeyboardRemove.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ig0rbm\Memo\Entity\Telegram\Keyboard;
+
+class ReplyKeyboardRemove
+{
+    public const KEY_NAME = 'remove_keyboard';
+
+    /** @var bool */
+    private $removeKeyboard = true;
+
+    /** @var bool */
+    private $selective = false;
+
+    public function isRemoveKeyboard(): bool
+    {
+        return $this->removeKeyboard;
+    }
+
+    public function setRemoveKeyboard(bool $removeKeyboard): void
+    {
+        $this->removeKeyboard = $removeKeyboard;
+    }
+
+    public function isSelective(): bool
+    {
+        return $this->selective;
+    }
+
+    public function setSelective(bool $selective): void
+    {
+        $this->selective = $selective;
+    }
+}
