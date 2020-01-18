8c1152efc2b7e390f6a6b05fc7032de8d5a8476b
1g0rbm <m1g0rb89@gmail.com>
2020-01-18 11:08:09 +0300

    Add command for quiz reminders

---
+++ src/Command/Quiz/RunQuizReminderCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ig0rbm\Memo\Command\Quiz;
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+use Ig0rbm\Memo\Entity\Telegram\Message\MessageTo;
+use Ig0rbm\Memo\Exception\Quiz\QuizStepException;
+use Ig0rbm\Memo\Repository\Quiz\QuizReminderRepository;
+use Ig0rbm\Memo\Service\Quiz\QuizManager;
+use Ig0rbm\Memo\Service\Quiz\QuizStepSerializer;
+use Ig0rbm\Memo\Service\Telegram\MessageBuilder;
+use Ig0rbm\Memo\Service\Telegram\TelegramApiService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunQuizReminderCommand extends Command
+{
+    private const NAME = 'memo:quiz_reminder:run';
+
+    private TelegramApiService $telegramApi;
+
+    private QuizReminderRepository $reminderRepository;
+
+    private QuizManager $quizManager;
+
+    private QuizStepSerializer $stepSerializer;
+
+    private MessageBuilder $builder;
+
+    public function __construct(
+        TelegramApiService $telegramApi,
+        QuizReminderRepository $reminderRepository,
+        QuizManager $quizManager,
+        QuizStepSerializer $stepSerializer,
+        MessageBuilder $builder
+    ) {
+        parent::__construct(self::NAME);
+
+        $this->telegramApi        = $telegramApi;
+        $this->reminderRepository = $reminderRepository;
+        $this->quizManager        = $quizManager;
+        $this->stepSerializer     = $stepSerializer;
+        $this->builder            = $builder;
+    }
+
+    public function configure(): void
+    {
+        $this->setName(self::NAME)
+            ->setDescription('Find and run reminders by time')
+            ->setHelp('The command should run by cron every minute and run reminder');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            '=========',
+            '  START  ',
+            '========='
+        ]);
+
+        $dt        = new DateTime('now', new DateTimeZone('UTC'));
+        $reminders = $this->reminderRepository->findAllEnabledByTime($dt->format('H:i'));
+
+        $output->writeln([sprintf('Found reminders for run: %d', count($reminders))]);
+
+        foreach ($reminders as $reminder) {
+            $quiz = $this->quizManager->getQuizByChat($reminder->getChat(), true);
+            $step = $quiz->getCurrentStep();
+
+            if (!isset($step)) {
+                throw QuizStepException::becauseThereAreNotQuizSteps($quiz->getId());
+            }
+
+            $this->builder->appendLn('Hi! Time to remember English!')
+                ->appendLn(sprintf('What is russian for "%s"', $step->getCorrectWord()->getText()));
+
+            $to = new MessageTo();
+            $to->setChatId($reminder->getChat()->getId());
+            $to->setText($this->builder->flush());
+            $to->setInlineKeyboard($this->stepSerializer->serialize($step));
+
+            $this->telegramApi->sendMessage($to);
+
+            $output->writeln([sprintf('Send quiz for chat %d', $reminder->getChat()->getId())]);
+        }
+
+        $output->writeln([
+            '=========',
+            '   END   ',
+            '========='
+        ]);
+    }
+}
--- src/Repository/Quiz/QuizReminderRepository.php
+++ src/Repository/Quiz/QuizReminderRepository.php
@@ -7,7 +7,6 @@ namespace Ig0rbm\Memo\Repository\Quiz;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\ORMException;
-use Ig0rbm\Memo\Entity\Quiz\Quiz;
 use Ig0rbm\Memo\Entity\Quiz\QuizReminder;
 use Ig0rbm\Memo\Entity\Telegram\Message\Chat;
 
@@ -16,6 +15,14 @@ class QuizReminderRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, QuizReminder::class);
+    }
+
+    /**
+     * @return QuizReminder[]
+     */
+    public function findAllEnabledByTime(string $time): array
+    {
+        return $this->findBy(['time' => $time, 'status' => QuizReminder::TYPE_ENABLE]);
     }
 
     public function findReminderByChatAndTime(Chat $chat, string $time): ?QuizReminder
